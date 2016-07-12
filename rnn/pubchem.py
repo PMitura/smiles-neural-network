87421725368483f4bdd8a50e350dad4003cf846f
Peter <pmitura.jr@gmail.com>
2016-07-12 10:18:12 +0200

    Testing of pubchempy API

---
+++ rnn/pubchem.py
@@ -0,0 +1,17 @@
+import pubchempy as pcp
+
+# Dummy function for testing capabilities of pubchempy
+def play():
+    aspirin = pcp.get_compounds('Aspirin', 'name')
+    for compound in aspirin:
+        print "{} has weight {}".format(compound.isomeric_smiles,
+                compound.molecular_weight)
+    print "Downloading CSV..."
+    pcp.download('CSV', 'test.csv', range(1, 10000), 
+            operation = 'property/CanonicalSMILES,MolecularWeight',
+            overwrite = True)
+    print "Done"
+
+
+if __name__ == '__main__':
+    play()
