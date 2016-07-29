c977b86cac2e7e5eb2cc4a9a53595f4f6ef0c22a
Peter <pmitura.jr@gmail.com>
2016-07-29 15:49:15 +0200

    Increase the number of epochs

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -24,7 +24,7 @@ from keras.optimizers import Adam, RMSprop
 SEED = 12346
 TD_LAYER_MULTIPLIER = 0.5   # Time-distributed layer modifier of neuron count
 GRU_LAYER_MULTIPLIER = 1    # -||- for GRU
-EPOCHS = 2
+EPOCHS = 150
 BATCH = 160                 # metacentrum.cz: 128 - 160, optimum by grid: 96
 LEARNING_RATE = 0.003
 EARLY_STOP = 50             # Number of tolerated epochs without improvement
