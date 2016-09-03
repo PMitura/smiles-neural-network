b811fcbdc2fc67ca7f6983c8a8eefcdc392035fe
Radovan Červený <radovan.cerveny@gmail.com>
2016-09-03 20:19:34 +0200

    pretrain model 50 features, 900k rows, fixed neurons, one more dense

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -60,11 +60,11 @@ def configureModel(input):
     #     trainable = RP['trainable_inner']),
     #     input_shape = (None, alphaSize )))
 
-    model.add(GRU(int(RP['gru_layer_multiplier'] * alphaSize), trainable = RP['trainable_inner'], input_shape = (None, alphaSize ), dropout_W=0.2,  dropout_U=0.2, return_sequences=True))
+    model.add(GRU(150, trainable = RP['trainable_inner'], input_shape = (None, alphaSize ), dropout_W=0.2,  dropout_U=0.2, return_sequences=True))
     # model.add(GRU(int(RP['gru_layer_multiplier'] * alphaSize), trainable = RP['trainable_inner'], dropout_W=0.2,  dropout_U=0.2, return_sequences = True ))
-    model.add(GRU(int(RP['gru_layer_multiplier'] * alphaSize), trainable = RP['trainable_inner'],W_regularizer=l2(0.01),U_regularizer=l2(0.01)))
+    model.add(GRU(150, trainable = RP['trainable_inner'],W_regularizer=l2(0.01),U_regularizer=l2(0.01)))
     model.add(Activation('relu', trainable = RP['trainable_inner']))
-    model.add(Dense(alphaSize) )
+    model.add(Dense(100) )
     model.add(Activation('relu', trainable = RP['trainable_inner']))
     model.add(Dense(outputLen) )
 
