6e595078a18b79faaf6917e470b99a7065e87cce
Radovan Červený <radovan.cerveny@gmail.com>
2016-09-07 10:13:47 +0200

    lesser dropout, regul tdd

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -73,11 +73,11 @@ def configureModel(input):
     # {'parameters_num': 0, 'name': 'dropout_2'}
     # {'output_dim': 53, 'parameters_num': 15953, 'activation': 'linear', 'name': 'dense_2', 'input_dim': None}
 
-    model.add(TimeDistributed(Dense(300, activation = 'tanh'), trainable = True, input_shape = (None, alphaSize )))
-    model.add(Dropout(0.55))
+    model.add(TimeDistributed(Dense(300, activation = 'tanh', W_regularizer=l2(0.00005)), trainable = True, input_shape = (None, alphaSize )))
+    model.add(Dropout(0.45))
     model.add(Bidirectional(GRU(300, trainable = True, W_regularizer=l2(0.00005),  b_regularizer=l2(0.00005))))
     model.add(Activation('relu'))
-    model.add(Dropout(0.55))
+    model.add(Dropout(0.45))
     model.add(Dense(outputLen) )
 
     if RP['classify']:
