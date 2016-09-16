67b51a1543b5d32b05671e4a08d193eed702ca54
Radovan Červený <radovan.cerveny@gmail.com>
2016-09-16 13:41:33 +0200

    td gru gru on maccs

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -76,6 +76,9 @@ def configureModel(input, outputLen = len(RD['labels'])):
     # model.add(GRU(int(RP['gru_layer_multiplier'] * alphaSize), trainable = RP['trainable_inner'], dropout_W=0.2,  dropout_U=0.2, return_sequences = True ))
     # model.add(TimeDistributed(Dense(300, activation = 'tanh', trainable = RP['trainable_inner']), input_shape = (None, alphaSize )))
 
+
+    model.add(TimeDistributed(Dense(300, activation = 'tanh', trainable = RP['trainable_inner']), input_shape = (None, alphaSize )))
+    model.add(Dropout(0.30))
     model.add(GRU(300, trainable = RP['trainable_inner'], input_shape = (None, alphaSize ), return_sequences = True))
     model.add(Activation('relu', trainable = RP['trainable_inner']))
     model.add(Dropout(0.30))
