0f5fb0b7f7551c2e53f795c6ceb7965cea11d8d7
Radovan Červený <radovan.cerveny@gmail.com>
2016-09-24 09:27:24 +0200

    continuing grid after crash

--- run.py
+++ run.py
@@ -49,7 +49,7 @@ def main(argv):
 
 
             # REMOVEME: hardcoded grid for neurons
-            grid = [300,350,400,450,500]
+            grid = [400,450,500]
             cc.exp['grid'] = {}
 
             import rnn.rnn
