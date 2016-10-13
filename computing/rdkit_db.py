6e73fb95e8dda3d13daea0e20fbcb2c7d75addd8
Radovan Červený <radovan.cerveny@gmail.com>
2016-10-13 21:23:09 +0200

    maccs computing

--- computing/rdkit_db.py
+++ computing/rdkit_db.py
@@ -19,9 +19,10 @@ LELIMIT = 10000
 
 SICHO_RIPTORS = Set(['MinAbsPartialCharge','HeavyAtomMolWt','MaxAbsPartialCharge','MinAbsEStateIndex','Chi3n','HallKierAlpha','PEOE_VSA1','PEOE_VSA10','PEOE_VSA11','PEOE_VSA12','PEOE_VSA13','PEOE_VSA14','PEOE_VSA2','PEOE_VSA3','PEOE_VSA6','PEOE_VSA8','PEOE_VSA9','SMR_VSA1','SMR_VSA10','SMR_VSA3','SMR_VSA6','SMR_VSA9','SlogP_VSA10','SlogP_VSA3','SlogP_VSA4','SlogP_VSA6','TPSA','EState_VSA3','EState_VSA5','EState_VSA7','EState_VSA8','VSA_EState9','NHOHCount','NumAliphaticHeterocycles','NumAromaticHeterocycles','MolLogP','fr_Ar_COO','fr_C_O','fr_Imine','fr_NH1','fr_Ndealkylation2','fr_amide','fr_aryl_methyl','fr_ester','fr_ether','fr_furan','fr_imidazole','fr_methoxy','fr_piperzine','fr_pyridine','fr_sulfide','fr_thiazole','fr_urea'])
 
-DOWNLOAD_TABLE = 'input.compound_structures'
-DOWNLOAD_COLS = ['molregno','canonical_smiles']
-SEND_TABLE = 'output.compounds_features_all'
+DOWNLOAD_TABLE = 'output.stitch_120ls_400lf_907d'
+DOWNLOAD_COLS = ['smiles']
+DOWNLOAD_QUERY = 'SELECT distinct {} FROM {} LIMIT {} OFFSET {}'
+SEND_TABLE = 'output.stitch_smiles_maccs'
 
 # DOWNLOAD_TABLE = 'output.target_geminin_deduplicated'
 # DOWNLOAD_COLS = ['molregno','canonical_smiles','is_testing','standard_value_min','standard_value_max','standard_value_count','standard_value_std','standard_value_relative_std','standard_value_median']
@@ -87,7 +88,7 @@ def computeMACCS(smilesDf):
 
 
 def getData(con, lo):
-    query = 'SELECT {} FROM {} where length(canonical_smiles) <= 80 LIMIT {} OFFSET {}'.format(
+    query = DOWNLOAD_QUERY.format(
         ','.join(['"{}"'.format(x) for x in DOWNLOAD_COLS]),
         DOWNLOAD_TABLE,
         LELIMIT,
@@ -98,8 +99,10 @@ def getData(con, lo):
         sql = query,
         con = con)
 
-    fingerDf = computeDescriptors(df)
-    # fingerDf = computeMACCS(df)
+    df.columns = ['canonical_smiles']
+
+    # fingerDf = computeDescriptors(df)
+    fingerDf = computeMACCS(df)
 
     mergedDf = pd.concat([df, fingerDf], axis=1)
 
