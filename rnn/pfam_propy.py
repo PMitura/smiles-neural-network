e61bb95d03433c69b5c965eaf9378fd7bc7ff228
Peter Mitura <pmitura.jr@gmail.com>
2016-10-30 23:29:25 +0100

    CSV output, 3kk setup

--- rnn/pfam_propy.py
+++ rnn/pfam_propy.py
@@ -6,18 +6,37 @@
 from propy import PyPro
 
 import db.db as db
+import csv
 
 def dbGet():
     return db.getData()
 
 def pfamToPropy():
     proteins = dbGet();
+
+    print '  Computing propy decriptors...'
+
     descArray = []
     for protein in proteins['pfam_sequence']:
         descObject = PyPro.GetProDes(protein)
         descriptors = descObject.GetCTD()
         descriptors['pfam_sequence'] = protein
         descArray.append(descriptors)
-    print descArray
 
+    print '  ...done'
+    print '  Writing CSV...'
+
+    with open('propy.csv', 'wb') as csvfile:
+        keys = []
+        writer = csv.writer(csvfile, delimiter=',')
+        for key in descArray[0].items():
+            keys.append(key[0])
+        writer.writerow(keys)
+        vals = []
+        for row in descArray:
+            for key, value in row.items():
+                vals.append(value)
+        writer.writerow(vals)
+
+    print '  ...done'
 
